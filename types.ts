e5dd4aa9f9a54de8ac5d4381ecc3f3f1dacb75d7
Edison Alvarez <ealvare@bu.edu>
2026-04-13 01:38:48 -0400

    added new dependencies and custom types

--- types.ts
+++ types.ts
@@ -1,16 +1,29 @@
-//
-export type DateProps = {
-    day: string;
-    month: string;
-    year: string; // maybe useful
-    hour: string;
-    minute: string;
-    // im not putting seconds
+
+// An invite to a collaborational calendar from a user to another user
+export type InviteProps = {
+    from: string;
+    to: string;
+    accepted: boolean;
 }
 
+// An event in a calendar
 export type EventProps = {
     id: string;
     name: string;
+    notes: string[]; // idk what else should be in an event
     start: Date;
     end: Date;
+}
+
+// A user's calendar
+export type CalendarProps = {
+    id: string;
+    name: string;
+    events: EventProps[];
+}
+
+export type UserProps = {
+    id: string;
+    username: string;
+    calendar: CalendarProps;
 }
